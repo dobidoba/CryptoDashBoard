affb4caf0383456294274b443da5570d74c64d84
CryptoDashboard <cryptocurrency.dashboard@gmail.com>
2017-11-09 13:00:15 +0100

    bug getallCurrencies

--- downloadCurrenciesHeure.php
+++ downloadCurrenciesHeure.php
@@ -1,7 +1,7 @@
   <?php
 		error_log ("downloadCurrenciesHeure.php START");
 		echo '<br/>'.date("Y-m-d H:i:s").' - GET currencies from https://api.coinmarketcap.com/v1/ticker/ <br/>';
-		$json = file_get_contents('https://api.coinmarketcap.com/v1/ticker/');
+		$json = file_get_contents('https://api.coinmarketcap.com/v1/ticker/?limit=2000');
 		$fp = fopen('currencies.json', 'w');
 		fwrite($fp, $json);
 		fclose($fp);
