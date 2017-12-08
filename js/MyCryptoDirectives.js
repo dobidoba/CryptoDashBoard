8b6e2a52693d04985c8eb73c9a4eb84df2393283
CryptoDashboard <cryptocurrency.dashboard@gmail.com>
2017-12-08 18:05:59 +0100

    fieldset

--- js/MyCryptoDirectives.js
+++ js/MyCryptoDirectives.js
@@ -8,9 +8,37 @@ app.directive("fieldsetToggler", function(){
         page: '@page'
 
       },
-      template: '<fieldset class="col col-xs-12">' +
-      '<legend> {{titre}} </legend>'+
-      '<div ng-transclude></div>'+
-      '</fieldset>'
+      template: '<fieldset  id="#legend" class="col col-xs-12">' +
+      '<legend ng-click="isHidden = !isHidden;"> <i class="fa fa-toggle-right" ng-show="isHidden"></i><i class="fa fa-toggle-down" ng-show="!isHidden"></i> {{titre}} </legend>'+
+      '<div class="fieldsetToggler" ng-transclude></div>'+
+      '</fieldset>',
+      controller: function($scope, $log){        
+        $("#legend").click(function(){
+          incr();
+        });
+        function incr(){
+          console.log('clic');
+          alert('toto');
+          
+        }
+      }
   };
+});
+
+app.directive('slideToggle', function() {  
+  return {
+    restrict: 'A',      
+    scope:{
+      isOpen: "=slideToggle"
+    },  
+    link: function(scope, element, attr) {
+      console.log('boum');
+      var slideDuration = parseInt(attr.slideToggleDuration, 10) || 200;      
+      scope.$watch('isOpen', function(newVal,oldVal){
+        if(newVal !== oldVal){ 
+          element.stop().slideToggle(slideDuration);
+        }
+      });
+    }
+  };  
 });
