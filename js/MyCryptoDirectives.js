ac86fdb4c002aacbce49e15b38819a7ad493356f
CryptoDashboard <cryptocurrency.dashboard@gmail.com>
2017-12-28 18:03:09 +0100

    go

--- js/MyCryptoDirectives.js
+++ js/MyCryptoDirectives.js
@@ -21,10 +21,12 @@ app.directive('slideToggle', function() {
       isOpen: "=slideToggle"
     },  
     link: function(scope, element, attr) {
-      var slideDuration = parseInt(attr.slideToggleDuration, 10) || 200;      
+
+      if(attr.slideToggleHide){element.hide();}
+      
       scope.$watch('isOpen', function(newVal,oldVal){
         if(newVal !== oldVal){ 
-          element.stop().slideToggle(slideDuration);
+          element.stop().slideToggle(200);
         }
       });
     }
